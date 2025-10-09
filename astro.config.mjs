faa03a69fd17c5c82067ed807aea3fa975ad1bdc
Valdis Arelis <arelisvaldis@gmail.com>
2025-10-09 10:24:56 +0300

    name changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://hkbertoson.github.io',
-    base: '/github-pages'
+    site: 'https://Areulius.github.io',
+    base: '/'
 });
